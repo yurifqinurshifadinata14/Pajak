378cf360f55c3866a3b77fc36a646c9c8bd15487
yurifqinurshifad14@gmail.com <114124146+yurifqinurshifadinata14@users.noreply.github.com>
2024-05-02 16:49:51 +0700

    tampilan & fungsi halaman pph

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\BerandaController;
 use App\Http\Controllers\PajakController;
 use App\Http\Controllers\JenisController;
+use App\Http\Controllers\PphController;
+use App\Http\Controllers\PphunifikasiController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -17,3 +19,17 @@ Route::get('/pajaksub', [PajakController::class, 'pajaksub'])->name('pajakSub');
 Route::get('/pajakEdit/{pajak}', [PajakController::class, 'edit'])->name('pajakEdit');
 Route::put('/pajakUpdate/{pajak}', [PajakController::class, 'update'])->name('pajakUpdate');
 Route::delete('/pajakDelete/{pajak}', [PajakController::class, 'destroy'])->name('pajakDestroy');
+
+Route::get('/pph', [PphController::class, 'index'])->name('pph');
+Route::post('/pphstore', [PphController::class, 'store'])->name('pphStore');
+Route::get('/pphsub', [PphController::class, 'pphsub'])->name('pphSub');
+Route::get('/pphEdit/{pph}', [PphController::class, 'edit'])->name('pphEdit');
+Route::put('/pphUpdate/{pph}', [PphController::class, 'update'])->name('pphUpdate');
+Route::delete('/pphDelete/{pph}', [PphController::class, 'destroy'])->name('pphDestroy');
+
+Route::get('/pphunifikasi', [PphunifikasiController::class, 'index'])->name('pphunifikasi');
+Route::post('/pphunifikasistore', [PphunifikasiController::class, 'store'])->name('pphunifikasiStore');
+Route::get('/pphunifikasisub', [PphunifikasiController::class, 'pphunifikasisub'])->name('pphunifikasiSub');
+Route::get('/pphunifikasiEdit/{pphunifikasi}', [PphunifikasiController::class, 'edit'])->name('pphunifikasiEdit');
+Route::put('/pphunifikasiUpdate/{pphunifikasi}', [PphunifikasiController::class, 'update'])->name('pphunifikasiUpdate');
+Route::delete('/pphunifikasiDelete/{pphunifikasi}', [PphunifikasiController::class, 'destroy'])->name('pphunifikasiDestroy');
