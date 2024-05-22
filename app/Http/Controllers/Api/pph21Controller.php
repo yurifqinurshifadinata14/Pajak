0a5b463848bab17e558bfe96d134d36fc725d5bf
lutfiangg20 <lutfiangg20@gmail.com>
2024-05-22 10:43:05 +0700

    as

--- app/Http/Controllers/Api/pph21Controller.php
+++ app/Http/Controllers/Api/pph21Controller.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\api;
 use App\Http\Controllers\Controller;
 use App\Models\Pph21;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class Pph21Controller extends Controller
 {
@@ -13,7 +14,7 @@ class Pph21Controller extends Controller
      */
     public function get()
     {
-        $pph21 =  Pph21::all();
+        $pph21 = Pph21::all();
         return response()->json([
             'pph21' => $pph21
         ]);
@@ -33,6 +34,33 @@ class Pph21Controller extends Controller
     public function store(Request $request)
     {
         //
+        $validated = Validator::make($request->all(), [
+            'id_pajak' => 'required',
+            'nik' => 'required',
+            'jumlah_bayar' => 'required',
+            'bpf' => 'required',
+            'biaya_bulan' => 'required',
+
+        ]);
+
+        if ($validated->fails()) {
+            return response()->json([
+                'message' => $validated->messages(),
+            ]);
+        } else {
+            PPh21::create([
+                'id_pajak' => $request->id_pajak,
+                'nik' => $request->nik,
+                'jumlah_bayar' => $request->jumlah_bayar,
+                'bpf' => $request->bpf,
+                'biaya_bulan' => $request->biaya_bulan,
+
+            ]);
+
+            return response()->json([
+                'message' => "Data telah tersimpan",
+            ]);
+        }
     }
 
     /**
@@ -41,7 +69,6 @@ class Pph21Controller extends Controller
     public function show(string $id)
     {
         //
-        $asdasdas;
     }
 
     /**
@@ -57,14 +84,50 @@ class Pph21Controller extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $validated = Validator::make($request->all(), [
+            'nik' => 'numeric',
+            'jumlah_bayar' => 'numeric',
+            'bpf' => 'numeric',
+            'biaya_bulan' => 'numeric',
+
+        ]);
+
+        if ($validated->fails()) {
+            return response()->json([
+                'message' => $validated->messages(),
+            ]);
+        } else {
+            $pph21 = Pph21::where('id', $id)->update([
+                'nik' => (int) $request->nik,
+                'jumlah_bayar' => (int) $request->jumlah_bayar,
+                'bpf' => (int) $request->bpf,
+                'biaya_bulan' => (int) $request->biaya_bulan,
+            ]);
+
+            return response()->json([
+                'message' => "Data telah tersimpan",
+            ]);
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy($id)
     {
         //
+        $pph21 = Pph21::where('id', $id)->first();
+
+        if (!$pph21) {
+            return response()->json([
+                'message' => "Data tidak bisa terhapus",
+            ]);
+        }
+
+        $pph21->delete();
+
+        return response()->json([
+            'message' => "Data telah terhapus",
+        ]);
     }
 }
