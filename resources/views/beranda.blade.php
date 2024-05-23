679b24195d840fd449439f4bfc43f13d0c2cf6e9
nadilakheisapa <nadilakheisapa@gmail.com>
2024-05-23 10:30:47 +0700

    add

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -1,116 +1,118 @@
 @extends ('main')
 @section('konten')
-    <main>
-                <!-- Begin Page Content -->
-                <div class="container-fluid">
+<main>
+    <!-- Begin Page Content -->
+    <div class="container-fluid">
 
-                    <!-- Page Heading -->
-                    <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                <h1 class="h3 mb-0 sidebar-brand-text font-color" style="font-size: 35px">Dashboard Wajib Pajak</h1>
-                        <!-- <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-navy shadow-sm"><i
+        <!-- Page Heading -->
+        <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 sidebar-brand-text font-color" style="font-size: 35px">Dashboard Wajib Pajak</h1>
+            <!-- <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-navy shadow-sm"><i
                                 class="fas fa-download fa-sm text-white-50"></i> Generate Report</a> -->
-                    </div>
+        </div>
 
-                    <h4>
-                        @if(Auth::check())
-                            Selamat Datang
-                            <b>{{ Auth::user()->name }}</b>,
-                            Anda Login sebagai
-                            <b>{{ Auth::user()->role }}</b>
-                        @else
-                            Anda belum login.
-                        @endif
-                    </h4>
-                    <br>
-                    <!-- Content Row -->
-                    <div class="row">
+        <h4>
+            @if(Auth::check())
+            Selamat Datang
+            <b>{{ Auth::user()->name }}</b>,
+            Anda Login sebagai
+            <b>{{ Auth::user()->role }}</b>
+            @else
+            Anda belum login.
+            @endif
+        </h4>
+        <br>
+        <!-- Content Row -->
+        <div class="row">
 
-                        <!-- Earnings (Monthly) Card Example -->
-                        <div class="col-xl-3 col-md-6 mb-4">
-                            <div class="card border-left-primary shadow h-100 py-2">
-                                <div class="card-body">
-                                    <div class="row no-gutters align-items-center">
-                                        <div class="col mr-2">
-                                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                                Jumlah Data Pembayar</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{$totalpembayar}}</div>
-                                        </div>
-                                        <div class="col-auto">
-                                            <i class="fas fa-calendar fa-2x text-gray-300"></i>
-                                        </div>
-                                    </div>
-                                </div>
+            <!-- Earnings (Monthly) Card Example -->
+            <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-primary shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                    Jumlah Data Pembayar</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{$totalpembayar}}</div>
                             </div>
-                        </div>
-
-                        <!-- Earnings (Monthly) Card Example -->
-                        <div class="col-xl-3 col-md-6 mb-4">
-                            <div class="card border-left-success shadow h-100 py-2">
-                                <div class="card-body">
-                                    <div class="row no-gutters align-items-center">
-                                        <div class="col mr-2">
-                                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                                Total Pembayaran PPH</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800">Rp {{ number_format($totalbayarpph, 0, ',', '.') }}</div>
-                                        </div>
-                                        <div class="col-auto">
-                                            <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <!--PPH 21 Card Example -->
-                        <div class="col-xl-3 col-md-6 mb-4">
-                            <div class="card border-left-info shadow h-100 py-2">
-                                <div class="card-body">
-                                    <div class="row no-gutters align-items-center">
-                                        <div class="col mr-2">
-                                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">
-                                                Total Pembayaran PPH21</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800">Rp {{ number_format($totalbayarpph21, 0, ',', '.') }}</div>
-                                        </div>
-                                        <div class="col-auto">
-                                            <i class="fas fa-comments fa-2x text-gray-300"></i>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <!-- Jumlah Karyawan Card Example -->
-                        <div class="col-xl-3 col-md-6 mb-4">
-                            <div class="card border-left-warning shadow h-100 py-2">
-                                <div class="card-body">
-                                    <div class="row no-gutters align-items-center">
-                                        <div class="col mr-2">
-                                            <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                                                Jumlah Karyawan</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{$jumlahkaryawan}}</div>
-                                        </div>
-                                        <div class="col-auto">
-                                            <i class="fas fa-comments fa-2x text-gray-300"></i>
-                                        </div>
-                                    </div>
-                                </div>
+                            <div class="col-auto">
+                                <i class="fas fa-user fa-2x text-gray-300"></i>
                             </div>
                         </div>
                     </div>
-                            <!-- Approach -->
-                            <div class="card shadow mb-4">
-                                <div class="card-header py-3">
-                                    <h6 class="m-0 font-color" style="font-size: 30px">Peraturan Perpajakan</h6>
-                                </div>
-                                <div class="card-body">
-                                    <p>SB Admin 2 makes extensive use of Bootstrap 4 utility classes in order to reduce
-                                        CSS bloat and poor page performance. Custom CSS classes are used to create
-                                        custom components and custom utility classes.</p>
-                                    <p class="mb-0">Before working with this theme, you should become familiar with the
-                                        Bootstrap framework, especially the utility classes.</p>
-                                </div>
-                            </div>
                 </div>
+            </div>
 
-    </main>
+            <!-- Earnings (Monthly) Card Example -->
+            <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-success shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                                    Total Pembayaran PPH</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">Rp
+                                    {{ number_format($totalbayarpph, 0, ',', '.') }}</div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!--PPH 21 Card Example -->
+            <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-info shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-info text-uppercase mb-1">
+                                    Total Pembayaran PPH21</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">Rp
+                                    {{ number_format($totalbayarpph21, 0, ',', '.') }}</div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-wallet fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Jumlah Karyawan Card Example -->
+            <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-warning shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
+                                    Jumlah Karyawan</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{$jumlahkaryawan}}</div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-comments fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Approach -->
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <h6 class="m-0 font-color" style="font-size: 30px">Peraturan Perpajakan</h6>
+            </div>
+            <div class="card-body">
+                <p>SB Admin 2 makes extensive use of Bootstrap 4 utility classes in order to reduce
+                    CSS bloat and poor page performance. Custom CSS classes are used to create
+                    custom components and custom utility classes.</p>
+                <p class="mb-0">Before working with this theme, you should become familiar with the
+                    Bootstrap framework, especially the utility classes.</p>
+            </div>
+        </div>
+    </div>
+
+</main>
 @endsection
