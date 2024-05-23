599e7d8cae2ec1a8782d1587d3131058ac1be861
lutfiangg20 <lutfiangg20@gmail.com>
2024-05-23 10:16:09 +0700

    fix component

--- resources/views/pajak/pajaksub.blade.php
+++ resources/views/pajak/pajaksub.blade.php
@@ -24,7 +24,7 @@
                     </div>
 
                     <!-- Modal Button Tambah -->
-                    <x-pajaksub.modalTambah />
+                    <x-pajaksub.modaltambah />
                     <!-- Modal Button Edit -->
                     <x-pajaksub.modaledit />
                     <!-- Modal Button import -->
