1d13c5b1c2347fed232335fd02d1a8ad878d6741
sofiainda <sofiaindam@gmail.com>
2024-05-24 10:37:25 +0700

    aa

--- app/Http/Controllers/PajakController.php
+++ app/Http/Controllers/PajakController.php
@@ -123,7 +123,7 @@ class PajakController extends Controller
         $pajak = Pajak::join('jenis', 'jenis.id_pajak', '=', 'pajaks.id_pajak')
             ->join('statuses', 'statuses.id_pajak', '=', 'pajaks.id_pajak')
             ->get();
-        dd($pajak);
+        // dd($pajak);
         return view('pajak.pajaksub', compact('pajak'));
     }
 
