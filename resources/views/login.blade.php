0ed022e3892324a004306fd8554e8028b79c8772
nirvanaivanta <nirvanaivanta@gmail.com>
2024-05-25 10:56:25 +0700

    add

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -106,11 +106,11 @@
                                         </a> --}}
                                     </form>
                                     <hr>
-                                    <div class="text-center">
+                                    <!-- <div class="text-center">
                                         <a class="small" href="/forgot-password">Forgot Password?</a>
-                                    </div>
+                                    </div> -->
                                     <div class="text-center">
-                                        <a class="small" href="/register">Create an Account!</a>
+                                        <a class="small" href="{{ route('register') }}">Create an Account!</a>
                                     </div>
                                 </div>
                             </div>
