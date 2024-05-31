2a12ebc0d8077cc91ebf6e7af3e8084411543036
lutfiangg20 <lutfiangg20@gmail.com>
2024-05-31 14:45:37 +0700

    datadmin

--- app/Http/Controllers/Api/DataadminController.php
+++ app/Http/Controllers/Api/DataadminController.php
@@ -77,7 +77,7 @@ class DataadminController extends Controller
 
     /**
      * Update the specified resource in storage.
-     */     
+     */
     public function update(Request $request, $id)
     {
         $validated = Validator::make($request->all(), [
@@ -87,33 +87,33 @@ class DataadminController extends Controller
             'password_confirmation' => 'nullable|string|min:5|max:255|same:password',
             'role' => 'string',
         ]);
-    
+
         if ($validated->fails()) {
             return response()->json([
                 'message' => $validated->messages(),
             ], 400); // Set status code 400 for bad request
         }
-    
+
         $dataadmin = User::findOrFail($id);
-    
+
         $data = [
             'name' => $request->has('name') ? $request->name : $dataadmin->name,
             'email' => $request->has('email') ? $request->email : $dataadmin->email,
             'role' => $request->has('role') ? $request->role : $dataadmin->role,
         ];
-    
+
         if ($request->filled('password')) {
             $data['password'] = Hash::make($request->password);
         }
-    
+
         $dataadmin->update($data);
-    
+
         return response()->json([
             'message' => "Data telah tersimpan",
         ]);
     }
-    
-    
+
+
 
     /**
      * Remove the specified resource from storage.
@@ -122,21 +122,20 @@ class DataadminController extends Controller
     {
         // Temukan data Pajak berdasarkan id_pajak
         $dataadmin = User::where('id', $id)->first();
-    
+
         // Periksa apakah data Pajak ditemukan
         if (!$dataadmin) {
             return response()->json([
                 'message' => "Data Admin dengan ID $id tidak ditemukan",
             ], 404);
         }
-    
+
         // Hapus data Pajak
         $dataadmin->delete();
-    
+
         // Kembalikan respons sukses
         return response()->json([
             'message' => "Data Admin dengan ID $id berhasil dihapus",
         ]);
     }
-    
 }
